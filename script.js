205280041b4c910cd33737ef9e1a2320c60142f6
Sudhanshu Tiwari <psiverg@gmail.com>
2022-11-06 04:14:12 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const hamburgerIcon = document.getElementById('hamburger-icon');
+
+hamburgerIcon.addEventListener('mousedown', toggleMobileMenu, false);
+hamburgerIcon.addEventListener('touchstart', toggleMobileMenu, false);
+
+let animateMenuKeyframes = new KeyframeEffect(
+  mobileMenu, 
+  [
+    { marginLeft: '100vw' }, 
+    { marginLeft: '63vw' },
+    { marginLeft: '25vw' },
+  ], 
+  { duration: 400 }
+);
+
+let reverseAnimateMenuKeyframes = new KeyframeEffect(
+  mobileMenu, 
+  [
+    { marginLeft: '25vw' }, 
+    { marginLeft: '63vw' },
+    { marginLeft: '100vw' },
+  ], 
+  { duration: 400 }
+);
+
+var mobileMenuAnimation = new Animation(animateMenuKeyframes);
+var reverseMobileMenuAnimation = new Animation(reverseAnimateMenuKeyframes);
+
+
+
+function toggleMobileMenu(){
+  hamburgerIcon.classList.toggle('open');
+  if(hamburgerIcon.classList.contains('open')){
+      mobileMenuAnimation.play();
+      setTimeout(setLeftMargin(true),400);
+  }else{
+      reverseMobileMenuAnimation.play();
+      setTimeout(setLeftMargin(false),400);
+  }
+}
+
+let setLeftMargin = (flag) => {
+  mobileMenu.style.marginLeft = flag ? '25vw' : '100vw';
+}
+
+
